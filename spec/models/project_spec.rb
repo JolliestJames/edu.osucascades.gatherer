62d4ef45fb683e28b03e4f027e57cfb595d69305
ToBeAmped <martinja@oregonstate.edu>
2018-04-05 20:19:12 -0700

    Fix project_spec.rb

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Project do
   it "considers a project with no tasks to be done" do
     project = Project.new
-    expect(project.done?).to be_truth
+    expect(project.done?).to be_truthy
   end
 
 end
