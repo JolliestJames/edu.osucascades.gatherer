1c50051480893f7c11e6062f800081f6c370cfe9
ToBeAmped <martinja@oregonstate.edu>
2018-04-10 23:58:29 -0700

    Refactor and update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,15 @@
 class Task
-  def initialize
-    @completed = false
+  attr_accessor :size, :completed
+
+  def initialize(options = {})
+    @completed = options[:completed]
+    @size = options[:size]
   end
+  
   def mark_completed
     @completed = true
   end
+  
   def complete?
     @completed
   end
