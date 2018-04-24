b0f0d3084309661dbd2d25af43a20941e721cadf
ToBeAmped <martinja@oregonstate.edu>
2018-04-24 13:18:46 -0700

    Add unhappy path feature test

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe "adding a project", type: :system do
     expect(page).to have_content("Project Runway")
     expect(page).to have_content(8)
   end
+
+  it "does not allow a user to create a project without a name" do
+    visit new_project_path
+    fill_in "Name", with: ""
+    fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5"
+    click_on("Create Project")
+    expect(page).to have_selector(".new_project")
+  end
+
 end
